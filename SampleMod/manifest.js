a8a062787ecd7d8d327c54e86a67200e0414eae9
Tethrarxitet <thesoul903@gmail.com>
2025-11-13 18:55:22 -0600

    create a sample mod manifest

---
+++ SampleMod/manifest.js
@@ -0,0 +1,10 @@
+{
+  "name": "Sample Mod",
+  "creators": ["Tethrarxitet"],
+      "//aa": "You can have more than one creator, but if you have one creator, this must",
+      "//ab": "still be a string array to prevent bugs.",
+  "descriptionPath": "readme.md",
+      "//b": "The description path will default to readme.md if no value is specified.",
+  "changelogPath": "changelog.md",
+      "//c": "The changelog path will default to changelog.md if no value is specified."
+}
