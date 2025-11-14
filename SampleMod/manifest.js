e114b423c932aabeffc8997aa5b53e79c1119ce2
Tethrarxitet <thesoul903@gmail.com>
2025-11-13 22:45:35 -0600

    update for clarification

--- SampleMod/manifest.js
+++ SampleMod/manifest.js
@@ -1,10 +1,12 @@
 {
   "name": "Sample Mod",
   "creators": ["Tethrarxitet"],
-      "//aa": "You can have more than one creator, but if you have one creator, this must",
-      "//ab": "still be a string array to prevent bugs.",
+
+  "//a": "All arguments below are optional; the files written are the defaults if",
+  "//b": "not reassigned.",
+
   "descriptionPath": "readme.md",
-      "//b": "The description path will default to readme.md if no value is specified.",
   "changelogPath": "changelog.md",
-      "//c": "The changelog path will default to changelog.md if no value is specified."
+  "icon": "icon.png",
+  "script": "script.js"
 }
